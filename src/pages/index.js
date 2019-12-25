ddd260ca5a36af3e2962435d9d09b1fe229cf06a
Shumpei Kishi <shumpei.kishi.dev@gmail.com>
2019-12-26 01:17:33 +0900

    Add blog list page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,44 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({data}) => {
+  return (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>クジラデジタルBlog</h1>
+    {
+      data.allMarkdownRemark.edges.map( ({node}) => {
+        return (
+          <div key={node.id}>
+            <Link to={`/${node.fields.slug}`}>
+              {node.frontmatter.title}
+            </Link>
+          </div>
+          
+        )
+      })
+    }
   </Layout>
-)
+)}
+
+export const query = graphql`
+  {
+    allMarkdownRemark {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+          }
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
