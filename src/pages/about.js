3369d9804f89bd2c474f73b4cf1d9430f840617e
Shumpei Kishi <shumpei.kishi@acquia.com>
2019-12-28 14:10:50 +0900

    Add about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,7 +8,8 @@ const AboutPage = ({ data }) => {
     <Layout>
       <SEO title="Kujira Digital | 著者について" />
       <h1>著者について</h1>
-      <p>Drupal Camp Den Nagoyaにて登壇しました。</p>
+      <p>カナダ・イエローナイフでウェブ開発者として勤務していた際、エンタープライズ向けオープンソースCMSのDrupalによる開発に従事。
+        帰国後、アクイア・Drupalのヘビーユーザーであるジョンソン・エンド・ジョンソン株式会社の社内SEを経て、2019年1月よりアクイアジャパン合同会社のSolusions Engineer。</p>
     </Layout>
   )
 }
