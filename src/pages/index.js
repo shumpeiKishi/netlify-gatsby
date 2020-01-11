c9d1f5e4802b79b56012028806674223c5ab8c97
Shumpei Kishi <shumpei.kishi.dev@gmail.com>
2020-01-11 14:28:21 +0900

    debug

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const IndexPage = ({ data }) => {
+  console.log(data)
   return (
     <Layout>
       <SEO title="Home" />
